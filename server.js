1d37bc19a349355422a4e19105c94be10c2c6c7b
Victor <papadoginc@gmail.com>
2016-11-14 20:42:51 +0300

    Add todo model and POST

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -46,18 +47,24 @@ app.get('/todos/:id',function(req,res){
 app.post('/todos',function(req,res){
     var body = _.pick(req.body, 'description', 'completed');
     
-    if(!_.isBoolean(body.completed) ||
-      !_.isString(body.description) ||
-      body.description.trim().length === 0){
-        return res.status(400).send();
-    }
+//    if(!_.isBoolean(body.completed) ||
+//      !_.isString(body.description) ||
+//      body.description.trim().length === 0){
+//        return res.status(400).send();
+//    }
+//    
+//    body.description = body.description.trim();
+//    body.id = nextTodoId;
+//    nextTodoId++;
+//    
+//    todos.push(body);    
+//    res.json(body);
     
-    body.description = body.description.trim();
-    body.id = nextTodoId;
-    nextTodoId++;
-    
-    todos.push(body);    
-    res.json(body);
+    db.todo.create(body).then(function(todo){
+        res.json(todo.toJSON());
+    },function(e){
+        res.status(400).json(e);
+    });
     
 });
 
@@ -99,6 +106,9 @@ app.put('/todos/:id',function(req, res){
     res.json(matchedTodo);
 })
 
-app.listen(PORT, function(){
+db.sequelize.sync().then(function(){
+   app.listen(PORT, function(){
     console.log("Server is running on port: " + PORT);
+}); 
 });
+
---
+++ vari.js
@@ -0,0 +1,8 @@
+var arr = [25,50];
+
+function updateArray(array){
+    array = [25, 50, 75];
+}
+
+updateArray(arr);
+console.log(array);
