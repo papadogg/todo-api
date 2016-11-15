97374aa09b37bde090aca056140623bbc92fbbf6
Victor <papadoginc@gmail.com>
2016-11-15 20:16:08 +0300

    Conver DELETE to sqlize

--- server.js
+++ server.js
@@ -73,16 +73,32 @@ app.delete('/todos/:id', function (req, res) {
     var todoId = parseInt(req.params.id, 10);
 
 
-    var matchedTodo = _.findWhere(todos, {
-        id: todoId
+    db.todo.destroy({
+        where: {
+            id: todoId
+        }
+    }).then(function(rowsDeleted){
+        if(rowsDeleted === 0){
+            res.status(404).json({
+               error: "No todo with id" 
+            });
+        } else {
+            res.status(204).send();
+        }
+        
+    },function(e){
+        res.status(500).send();
     });
-
-    if (matchedTodo) {
-        todos = _.without(todos, matchedTodo);
-        res.json(matchedTodo);
-    } else {
-        res.status(404).send();
-    }
+//    var matchedTodo = _.findWhere(todos, {
+//        id: todoId
+//    });
+//
+//    if (matchedTodo) {
+//        todos = _.without(todos, matchedTodo);
+//        res.json(matchedTodo);
+//    } else {
+//        res.status(404).send();
+//    }
 });
 
 app.put('/todos/:id', function (req, res) {
